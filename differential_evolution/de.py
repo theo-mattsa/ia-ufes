0fba0439e9adaead39d9888e4141ae147add4726
Theo <theomatteo.sa@gmail.com>
2026-05-24 16:05:12 -0300

    update differential evolution

--- differential_evolution/de.py
+++ differential_evolution/de.py
@@ -1,9 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-
-PENALTY = 10_000
-DIMENSION = 30
 
 
 def ackley(x: np.ndarray) -> float:
@@ -18,128 +13,59 @@ def ackley(x: np.ndarray) -> float:
     return term1 + term2 + a + np.exp(1)
 
 
-def fitness(individual: np.ndarray):
-    cost = ackley(individual)
-    penalty = PENALTY if np.any(individual < -32) or np.any(individual > 32) else 0
-    return cost + penalty
-
-
-def create_population(population_size: int) -> np.ndarray:
-    return np.random.uniform(-32, 32, size=(population_size, DIMENSION))
-
-
-def mutation(population: np.ndarray, factor: float):
-    i1, i2, i3 = np.random.choice(len(population), 3, replace=False)
-    return population[i1] + factor * (population[i2] - population[i3])
-
-
-def crossover(population: np.ndarray, mutant: np.ndarray, rate: float):
-    new_i = []
-    i1_idx = np.random.choice(len(population), replace=False)
-    i1 = population[i1_idx]
-    for i in range(len(i1)):
-        if np.random.random() <= rate:
-            new_i.append(mutant[i])
-        else:
-            new_i.append(i1[i])
-
-    return np.array(new_i)
-
-
-def de(
-    generations: int = 100,
-    population_size: int = 50,
-    mutation_factor: float = 0.4,
-    crossover_rate: float = 0.7,
+def differential_evolution(
+    func,
+    bounds,
+    dimensions,
+    population_size=50,
+    F=0.4,
+    CR=0.7,
+    generations=100,
 ):
-    history = []
-    population = create_population(population_size)
+    rng = np.random.default_rng()
 
-    for generation in range(generations):
+    min_bound, max_bound = bounds
 
-        fitnesses = np.array([fitness(ind) for ind in population])
+    population = rng.uniform(min_bound, max_bound, size=(population_size, dimensions))
 
-        best_idx = np.argmin(fitnesses)
-        score = fitnesses[best_idx]
-        history.append(score)
+    fitness = np.asarray([func(ind) for ind in population])
+    best_idx = np.argmin(fitness)
+    best = population[best_idx]
 
-        # print(f"Generation: {generation}, score: {score}")
+    for gen in range(generations):
+        for i in range(population_size):
 
-        new_population = []
-
-        for _ in range(population_size):
+            # Choose 3 random individuals
+            idxs = [idx for idx in range(population_size) if idx != i]
+            a, b, c = population[rng.choice(idxs, 3, replace=False)]
 
             # Mutation
-            mutant = mutation(population, mutation_factor)
+            mutant = np.clip(a + F * (b - c), min_bound, max_bound)
 
             # Crossover
-            new_individual = crossover(population, mutant, crossover_rate)
+            mask = rng.random(dimensions) < CR
+            if not np.any(mask):
+                mask[rng.randint(0, dimensions)] = True
+
+            trial = np.where(mask, mutant, population[i])
 
             # Selection
-            random_idx = np.random.choice(len(population))
-            random_ind = population[random_idx]
+            f = func(trial)
 
-            if fitness(new_individual) < fitness(random_ind):
-                new_population.append(new_individual)
-            else:
-                new_population.append(random_ind)
+            if f < fitness[i]:
+                population[i] = trial
+                fitness[i] = f
+                if f < fitness[best_idx]:
+                    best_idx = i
+                    best = trial
+        print(f"Generation: {gen + 1}: Best fitness = {fitness[best_idx]}")
 
-        population = new_population
-    return history
-
-
-def experiment(
-    iterations: int,
-    generations: int,
-    population_size: int,
-    cross_rate: float,
-    mutation_factor: float,
-):
-    results = []
-    print(
-        "Experiment: Generations:",
-        generations,
-        "Crossover Rate:",
-        cross_rate,
-        "Mutation Factor:",
-        mutation_factor,
-        "Population Size:",
-        population_size,
-    )
-    for _ in tqdm(range(iterations)):
-        history = de(
-            generations=generations,
-            population_size=population_size,
-            mutation_factor=mutation_factor,
-            crossover_rate=cross_rate,
-        )
-        results.append(history)
-
-    return results
+    return best, fitness[best_idx]
 
 
 if __name__ == "__main__":
-
-    results = experiment(
-        iterations=5,
-        generations=120,
-        population_size=80,
-        cross_rate=0.7,
-        mutation_factor=0.4,
+    best, fitness = differential_evolution(
+        ackley, bounds=(-32, 32), dimensions=30, population_size=100, generations=250
     )
-
-    final_scores = np.array([h[-1] for h in results])
-
-    print("Mean:", np.mean(final_scores))
-    print("Std:", np.std(final_scores))
-    print("Best:", np.min(final_scores))
-
-    # Plot
-    for history in results:
-        plt.plot(history)
-
-    plt.xlabel("Generation")
-    plt.ylabel("Fitness")
-    plt.title("Differential Evolution")
-
-    plt.show()
+    print(best)
+    print(fitness)
